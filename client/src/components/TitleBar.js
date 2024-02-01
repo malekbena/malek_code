359fa981a04950fab23fc08627c8dc92065a3952
malekbena <malekb.benali@gmail.com>
2024-02-01 12:05:02 +0100

    theme colors + dl icons svg

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -1,10 +1,33 @@
+import Image from 'next/image'
 import styles from '../styles/TitleBar.module.css'
 
 const TitleBar = () => {
     return (
-        <div className={styles.titlebar}>
-            <h1>Malek Ben Ali Portfolio</h1>
-        </div>
+        <section className={styles.titlebar}>
+            <Image
+                src="/vscode_icon.svg"
+                alt="VSCode Icon"
+                height={15}
+                width={15}
+                className={styles.icon}
+            />
+            <div className={styles.items}>
+                <p>File</p>
+                <p>Edit</p>
+                <p>Selection</p>
+                <p>View</p>
+                <p>Go</p>
+                <p>Run</p>
+                <p>Terminal</p>
+                <p>Help</p>
+            </div>
+            <h1 className={styles.title}>Malek Ben Ali - Visual Studio Code</h1>
+            <div className={styles.windowButtons}>
+                <span className={styles.minimize}></span>
+                <span className={styles.maximize}></span>
+                <span className={styles.close}></span>
+            </div>
+        </section>
     )
 }
 
