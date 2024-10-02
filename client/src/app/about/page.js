4d7ee28cfbf3795a61691e73e5c4b97aee4f40b5
malekbena <malekb.benali@gmail.com>
2024-10-02 12:43:18 +0200

    about page build error

--- client/src/app/about/page.js
+++ client/src/app/about/page.js
@@ -4,11 +4,11 @@ const AboutPage = () => {
             <h3>A propos de moi</h3>
             <p>Dans le monde du développement depuis 2019.
                 Après une formation en Programmation Orientée Objet, je me suis lancé en free-lance entre 2019 et 2022.
-                Je me suis formé a ReactJS, php et mysql principalement en autodidacte. J&apos;ai eu l'opportunité de travailler sur divers projets, tels que des blogs,
-                des sites vitrines d'agences immobilières ou encore de salles de sport et des fonctionnalités pour des sites e-commerce.
-                J&apos;ai ensuite intégré une formation qualifiante OpenClassrooms afin de valider mes acquis front end.
-                J&apos;ai profité de cette formation pour me spécialiser en JavaScript et me former à NodeJS en amont avec l&apos;appui de mon mentor de formation.
-                Aujourd&apos;hui je suis à la recherche d&apos;un poste de développeur ReactJS et/ou NodeJs.</p>
+                Je me suis formé a ReactJS, php et mysql principalement en autodidacte. J&rsquo;ai eu l&rsquo;opportunité de travailler sur divers projets, tels que des blogs,
+                des sites vitrines d&rsquo;agences immobilières ou encore de salles de sport et des fonctionnalités pour des sites e-commerce.
+                J&rsquo;ai ensuite intégré une formation qualifiante OpenClassrooms afin de valider mes acquis front end.
+                J&rsquo;ai profité de cette formation pour me spécialiser en JavaScript et me former à NodeJS en amont avec l&rsquo;appui de mon mentor de formation.
+                Aujourd&rsquo;hui je suis à la recherche d&rsquo;un poste de développeur ReactJS et/ou NodeJs.</p>
         </div>
     )
 }
